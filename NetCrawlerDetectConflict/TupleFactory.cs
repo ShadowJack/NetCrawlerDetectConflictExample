3237a6b5eb1c0c478e5534c250a403fbd64657f4
ShadowJack <konstantin.pavlovsky@listglobally.com>
2019-06-26 15:52:49 +0300

    Working example

---
+++ NetCrawlerDetectConflict/TupleFactory.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetCrawlerDetectConflict
+{
+    public static class TupleFactory
+    {
+        public static (int first, int second) Build()
+        {
+            var rng = new Random();
+            return (rng.Next(), rng.Next());
+        }
+
+    }
+}
