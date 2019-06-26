c85d0d4dd5a2fafe8a4be2f9ae5a8a9255748974
ShadowJack <konstantin.pavlovsky@listglobally.com>
2019-06-26 15:54:01 +0300

    Broken build

---
+++ NetCrawlerDetectConflict/BotDetector.cs
@@ -0,0 +1,21 @@
+﻿using NetCrawlerDetect;
+
+namespace NetCrawlerDetectConflict
+{
+    public class BotDetector
+    {
+        public string Check(string userAgent)
+        {
+            var detector = new CrawlerDetect();
+            var isBot = detector.IsCrawler(userAgent);
+
+            if (isBot)
+            {
+                return detector.Matches[0].Value;
+            }
+
+            return null;
+        }
+
+    }
+}
